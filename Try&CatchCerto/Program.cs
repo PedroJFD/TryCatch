c373eb4d65300f5e08ba78b88c4f9e78f0a706a7
Pedro Joaquim <pedro.joaquim@estudante.ifro.edu.br>
2023-06-05 17:01:54 -0400

    second commit

--- Try&CatchCerto/Program.cs
+++ Try&CatchCerto/Program.cs
@@ -1,18 +1,23 @@
 ﻿using System;
 
-Console.WriteLine("Informe o promeiro valor!");
-int a = Convert.ToInt32(Console.ReadLine());
-
-Console.WriteLine("Informe o promeiro valor!");
-int b = Convert.ToInt32(Console.ReadLine());
-
-int r;
-
 try
 {
-    r = a / b;
+    Console.WriteLine("Informe o promeiro valor!");
+    int a = Convert.ToInt32(Console.ReadLine());
+
+    Console.WriteLine("Informe o promeiro valor!");
+    int b = Convert.ToInt32(Console.ReadLine());
+
+    int r = a / b;
     Console.WriteLine($"O resultado é {r}");
-}catch(Exception e) 
+}catch (DivideByZeroException e)
 {
-    Console.WriteLine("Não é permitido divisão por zero!");
+    Console.WriteLine($"Não é permitido divisão por zero! {e.Message}");
+}catch (InvalidCastException e)
+{
+    Console.WriteLine($"Não é permitido divisão por zero! {e.Message}");
+}
+catch (Exception e)
+{
+    Console.WriteLine($"Não é permitido divisão por zero! {e.Message}");
 }
