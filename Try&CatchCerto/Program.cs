eb14c76e6e98e7d01f6df137a9b8c016e2321bf8
Pedro Joaquim <pedro.joaquim@estudante.ifro.edu.br>
2023-06-05 16:45:53 -0400

    first commit

---
+++ Try&CatchCerto/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+Console.WriteLine("Informe o promeiro valor!");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Informe o promeiro valor!");
+int b = Convert.ToInt32(Console.ReadLine());
+
+int r;
+
+try
+{
+    r = a / b;
+    Console.WriteLine($"O resultado é {r}");
+}catch(Exception e) 
+{
+    Console.WriteLine("Não é permitido divisão por zero!");
+}
